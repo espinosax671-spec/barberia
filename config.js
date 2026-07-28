b7826a9bb9df28e0a07b2200d4bd4c5c7306c288
espinosax671-spec <espinosax671@gmail.com>
2026-07-28 17:48:37 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // ⚠️ Reemplaza con tus credenciales de Supabase
-const SUPABASE_URL = 'https://XXXXX.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGc...';
+const SUPABASE_URL = 'https://gmoulnbrszgheajookcf.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdtb3VsbmJyc3pnaGVham9va2NmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUxNjU1NzcsImV4cCI6MjEwMDc0MTU3N30.COU_8svOgM-301oxLqqfmqSj3gwQ5Y2x9zz-cPWaSu4';
 
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
